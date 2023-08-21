1fc810987102828e21a6583af1e5b69dd58beb3e
Mcu_sdk Ci <mcu_sdk.ci@nxp.com>
2023-08-21 17:55:57 +0200

    Apply update for 2.14.0 EVKCMIMXRT1060

--- set_middleware_eiq.cmake
+++ set_middleware_eiq.cmake
@@ -1434,6 +1434,35 @@ endif()
 endif()
 
 
+if (CONFIG_USE_middleware_eiq_worker_video_evkcmimxrt1060)
+# Add set(CONFIG_USE_middleware_eiq_worker_video_evkcmimxrt1060 true) in config.cmake to use this component
+
+message("middleware_eiq_worker_video_evkcmimxrt1060 component is included from ${CMAKE_CURRENT_LIST_FILE}.")
+
+if(CONFIG_USE_middleware_eiq_worker)
+
+target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
+  ${CMAKE_CURRENT_LIST_DIR}/./common/video/camera/MT9M114_OV7725/eiq_camera_conf.c
+  ${CMAKE_CURRENT_LIST_DIR}/./common/video/display/RK043FN02HC/eiq_display_conf.c
+  ${CMAKE_CURRENT_LIST_DIR}/./common/video/eiq_camera.c
+  ${CMAKE_CURRENT_LIST_DIR}/./common/video/eiq_display.c
+  ${CMAKE_CURRENT_LIST_DIR}/./common/video/eiq_pxp.c
+  ${CMAKE_CURRENT_LIST_DIR}/./common/video/eiq_video_worker.c
+)
+
+target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
+  ${CMAKE_CURRENT_LIST_DIR}/./common/video
+)
+
+else()
+
+message(SEND_ERROR "middleware_eiq_worker_video_evkcmimxrt1060 dependency does not meet, please check ${CMAKE_CURRENT_LIST_FILE}.")
+
+endif()
+
+endif()
+
+
 if (CONFIG_USE_middleware_eiq_worker_video_evkbimxrt1050)
 # Add set(CONFIG_USE_middleware_eiq_worker_video_evkbimxrt1050 true) in config.cmake to use this component
 
